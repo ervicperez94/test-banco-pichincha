b0c224fd7bbd9a405233b0d790936a3d62192f59
Ervic Pérez Mendoza <ervic@puller.mx>
2023-05-24 20:10:29 -0600

    se agrega middleware para manejo de apikey

--- src/api-key/api-key.middleware.ts
+++ src/api-key/api-key.middleware.ts
@@ -4,10 +4,10 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class ApiKeyMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    const apiKey = req.headers['api-key'];
+    const apiKey = req.headers['x-parse-rest-api-key'];
 
-    if (!apiKey) {
-      return res.status(401).json({ message: 'Missing API key' });
+    if (!apiKey || apiKey !== '2f5ae96c-b558-4c7b-a590-a501ae1c3f6c') {
+      return res.status(401).json({ message: 'Invalid API key' });
     }
 
     next();
